de1259b9fba402f394cd54767e9c67ee7e80e65c
Avraham Appel <aappel248@gmail.com>
2024-09-29 12:30:34 -0400

    Update cargo hash on update

--- update.sh
+++ update.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
 # Update flake
 nix flake update
 
 # Update cargo
 cargo update
+
+# Update cargo hash if necessary
+if ! output="$(nix build --no-link |& tee /dev/tty)"; then
+    # Find updated hash
+    new_hash="$(echo "$output" | awk '/got:/ {print $2}')"
+    echo "Updating Cargo dependencies hash to [$new_hash]"
+
+    sed -i "s/cargoHash = \".*\";/cargoHash = \"$new_hash\";/" default.nix
+fi
