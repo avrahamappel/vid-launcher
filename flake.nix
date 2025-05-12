ba7050111e352a869f76523f5d1525a2239724ca
Avraham Appel <aappel248@gmail.com>
2025-05-12 19:48:06 -0400

    Remove fenix and bump version

--- flake.nix
+++ flake.nix
@@ -2,14 +2,11 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    fenix.url = "github:nix-community/fenix";
-    fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     { flake-utils
     , nixpkgs
-    , fenix
     , ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -17,32 +14,23 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ fenix.overlays.default ];
-        };
-
-        cargoDeps = pkgs.rustPlatform.importCargoLock {
-          lockFile = ./Cargo.lock;
         };
       in
       {
         defaultPackage = pkgs.callPackage ./. { };
 
         devShell = pkgs.mkShell {
-          inherit cargoDeps;
           nativeBuildInputs = with pkgs; [
-            rustPlatform.cargoSetupHook
             rust-analyzer
-            (pkgs.fenix.stable.withComponents [
-              "cargo"
-              "clippy"
-              "rust-src"
-              "rustc"
-              "rustfmt"
-            ])
             glib
             gtk4
             pkg-config
             cargo-bump
+            cargo
+            clippy
+            rustc
+            rustfmt
+            bacon
           ];
         };
       }
