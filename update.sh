47ea05f73a5056a8dc81b926e93439bd24b56d88
Avraham Appel <aappel248@gmail.com>
2025-07-13 13:53:32 -0400

    Update every Sunday, but only bump version if Cargo deps changed

--- update.sh
+++ update.sh
@@ -6,8 +6,8 @@ nix flake update
 # Update cargo
 cargo update
 
-# Update patch version if dependencies changed
-if [[ -n "$(git diff --name-only)" ]]; then
+# Update patch version if Cargo dependencies changed
+if [[ "$(git diff --name-only)" =~ Cargo.lock ]]; then
   cargo bump patch
   cargo check # So the change will propagate to Cargo.lock
 fi
