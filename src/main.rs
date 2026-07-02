55b5b0c10f0f42e25784e459ac4ff27c97ead181
Avraham Appel <aappel248@gmail.com>
2026-07-02 01:37:08 -0400

    Do fetching as init task

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod components;
 mod file_operations;
 mod weights;
 
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::time::Duration;
 
 use async_io::Timer;
@@ -18,31 +18,7 @@ use crate::{
     file_operations::{get_open_command, get_subdirectories, get_video_files},
 };
 
-fn play_random_video(directory: &Path) -> Task<Event> {
-    let video_files = get_video_files(directory);
-    if let Ok(random_video) =
-        video_files.choose_weighted(&mut rand::rng(), crate::weights::weight_by_last_accessed)
-    {
-        let cmd = get_open_command();
-
-        Task::perform(Command::new(cmd).arg(random_video).status(), |res| {
-            Event::Complete(res.map(|_| ()).map_err(|e| e.kind().to_string()))
-        })
-    } else {
-        Task::done(Event::Complete(
-            Err("No video files found in folder".into()),
-        ))
-    }
-}
-
-fn open_folder(directory: &PathBuf) -> Task<Event> {
-    let cmd = get_open_command();
-
-    Task::perform(Command::new(cmd).arg(directory).status(), |res| {
-        Event::Complete(res.map(|_| ()).map_err(|e| e.kind().to_string()))
-    })
-}
-
+#[derive(Clone)]
 struct Show {
     name: String,
     path: PathBuf,
@@ -64,16 +40,9 @@ struct App {
 
 impl App {
     fn new() -> Self {
-        // TODO do this work on load so we show window right away
-        let home_dir = std::env::var("HOME").expect("Failed to get HOME environment variable");
-        let videos_dir = PathBuf::from(home_dir).join("Videos");
-
-        let directories = get_subdirectories(&videos_dir);
-        let shows = directories.into_iter().filter_map(Show::new).collect();
-
         Self {
-            shows,
-            loading: false,
+            shows: vec![],
+            loading: true,
             error: None,
         }
     }
@@ -82,24 +51,82 @@ impl App {
 #[derive(Clone)]
 #[non_exhaustive]
 enum Event {
+    ShowsLoaded(Vec<Show>),
     PlayRandomVideo(usize),
     BrowseShow(usize),
     Complete(Result<(), String>),
     ClearError,
 }
 
+/// Load all shows
+async fn get_shows() -> Vec<Show> {
+    // TODO load preconfigured shows
+
+    // Load all directories in Videos
+    let home_dir = std::env::var("HOME").expect("Failed to get HOME environment variable");
+    let videos_dir = PathBuf::from(home_dir).join("Videos");
+
+    let directories = get_subdirectories(&videos_dir);
+
+    directories.into_iter().filter_map(Show::new).collect()
+}
+
+/// Play a randomly selected video in the given directory,
+/// preferring less recently accessed files
+async fn play_random_video(directory: PathBuf) -> Result<(), String> {
+    let video_files = get_video_files(&directory);
+
+    // Call the rng within a block so it's dropped before we need to switch threads
+    let try_random_video =
+        { video_files.choose_weighted(&mut rand::rng(), crate::weights::weight_by_last_accessed) };
+
+    if let Ok(random_video) = try_random_video {
+        let cmd = get_open_command();
+
+        Command::new(cmd)
+            .arg(random_video)
+            .status()
+            .await
+            .map_err(|e| e.kind().to_string())?;
+        Ok(())
+    } else {
+        Err("No video files found in folder".into())
+    }
+}
+
+async fn open_folder(directory: PathBuf) -> Result<(), String> {
+    let cmd = get_open_command();
+
+    Command::new(cmd)
+        .arg(directory)
+        .status()
+        .await
+        .map_err(|e| e.kind().to_string())?;
+
+    Ok(())
+}
+
+fn init() -> (App, Task<Event>) {
+    (App::new(), Task::perform(get_shows(), Event::ShowsLoaded))
+}
+
 fn update(app: &mut App, event: Event) -> Task<Event> {
     use Event::*;
     match event {
+        ShowsLoaded(shows) => {
+            app.shows = shows;
+            app.loading = false;
+            Task::none()
+        },
         PlayRandomVideo(idx) => {
             app.loading = true;
-            let show = &app.shows[idx];
-            play_random_video(&show.path)
+            let show_path = app.shows[idx].path.clone();
+            Task::perform(play_random_video(show_path), Event::Complete)
         },
         BrowseShow(idx) => {
             app.loading = true;
-            let show = &app.shows[idx];
-            open_folder(&show.path)
+            let show_path = app.shows[idx].path.clone();
+            Task::perform(open_folder(show_path), Event::Complete)
         },
         Complete(res) => {
             app.loading = false;
@@ -159,7 +186,7 @@ fn view(app: &App) -> Column<'_, Event> {
 }
 
 fn main() -> iced::Result {
-    iced::application(App::new, update, view)
+    iced::application(init, update, view)
         .title(option_env!("VID_LAUNCHER_TITLE").unwrap_or("vid-launcher-debug"))
         .window_size((300, 400))
         .run()
